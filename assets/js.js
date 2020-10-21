b7dbe549d97ed852dc22ac75d3dead9044d547b8
itsabdiev <abdiev0225@gmail.com>
2020-10-21 22:20:40 +0300

    our team
    tg

--- assets/js.js
+++
@@ -1,8 +0,0 @@
-var $toggleButton = $(".toggle-button"),
-  $menuWrap = $(".menu-wrap");
-
-$toggleButton.on("click", function() {
-  $(this).toggleClass("button-open");
-  $menuWrap.toggleClass("menu-show");
-});
-
