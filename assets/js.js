ce5efbeb22c13411a1208724f94f4fbdafb8b3fd
itsabdiev <abdiev0225@gmail.com>
2020-10-31 15:39:43 +0300

    л
    тлл

--- assets/js.js
+++ assets/js.js
@@ -25,3 +25,38 @@ $("#contact27").click(function() {
         scrollTop: $("#cucum").offset().top
     }, 2300);
 });
+
+
+
+
+
+$(document).ready(function() {
+
+    $('.search').each(function() {
+        var self = $(this);
+        var div = self.children('div');
+        var placeholder = div.children('input').attr('placeholder');
+        var placeholderArr = placeholder.split(/ +/);
+        if(placeholderArr.length) {
+            var spans = $('<div />');
+            $.each(placeholderArr, function(index, value) {
+                spans.append($('<span />').html(value + '&nbsp;'));
+            });
+            div.append(spans);
+        }
+        self.click(function() {
+            self.addClass('open');
+            setTimeout(function() {
+                self.find('input').focus();
+            }, 750);
+        });
+        $(document).click(function(e) {
+            if(!$(e.target).is(self) && !jQuery.contains(self[0], e.target)) {
+                self.removeClass('open');
+            }
+        });
+    });
+
+});
+
+
